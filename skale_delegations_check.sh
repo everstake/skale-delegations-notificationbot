5f95969737292a66e0677c553afabd88fe01c1cc
Nikolay-everstake <68022468+Nikolay-everstake@users.noreply.github.com>
2021-04-02 11:25:43 +0300

    Update skale_delegations_check.sh

--- skale_delegations_check.sh
+++ skale_delegations_check.sh
@@ -1,26 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
 sendmessage() 
 {
         for ID in $CHATIDS; do
-                curl -s -X POST https://api.telegram.org/bot$BOTKEY/sendMessage -d chat_id=${ID} -d text="$1" -d parse_mode="Markdown"
+                curl -s -X POST https://api.telegram.org/bot$BOTKEY/sendMessage -d chat_id=${ID} -d text="$1" -d parse_mode="html"
         done
 }
 
 delegate_check () 
 {
-   NEWDELEGATIONS=$(sk-val validator delegations $VALID | grep "$STATUS" | sed 's/$/\n/');
-   if [ -z $NEWDELEGATIONS ]; then
-      :
-   else
-      sendmessage "👀 NEW SKALE DELEGATIONS %0A%0A $NEWDELEGATIONS"
-   fi
+   for STATUS in $STATUSES; do
+      DELEGATIONS=$(sk-val validator delegations $VALID | grep "$STATUS" | awk '{print $3}' | sed 's/$/\n/');
+      NUM=$(sk-val validator delegations $VALID | grep "$STATUS" | sed 's/$/\n/');
+         if [ -z $DELEGATIONS ]; then
+            :
+         elif [ "$DELEGATIONS" == "PROPOSED" ]; then
+            sendmessage "👀 NEW SKALE DELEGATIONS %0A%0A $NUM"
+         elif [ "$DELEGATIONS" == "UNDELEGATION_REQUESTED" ]; then 
+            sendmessage "👀 NEW SKALE UNDELEGATIONS %0A%0A $NUM"
+         fi
+   done
 }
 
 CONFIG="skale_delegations_bot.ini"
 while true; do
    CHATIDS=$(cat ${PWD}/${CONFIG} | grep -v "#" | grep "CHATIDS" | awk -F "=" '{print $2}');
    BOTKEY=$(cat ${PWD}/${CONFIG} | grep -v "#" | grep "BOTKEY" | awk -F "=" '{print $2}');
-   STATUS=$(cat ${PWD}/${CONFIG} | grep -v "#" | grep "STATUS" | awk -F "=" '{print $2}');
+   STATUSES=$(cat ${PWD}/${CONFIG} | grep -v "#" | grep "STATUSES" | awk -F "=" '{print $2}');
    VALID=$(cat ${PWD}/${CONFIG} | grep -v "#" | grep "VALID" | awk -F "=" '{print $2}');
    delegate_check
    sleep 14400;
